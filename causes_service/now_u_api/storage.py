e9ad161df1e3a45a4717cc3594055477b2699688
James Elgar <jamesnelgar@gmail.com>
2024-07-21 09:44:59 +0100

    Allow setting custom domain for azure storage (for cdn)

--- causes_service/now_u_api/storage.py
+++ causes_service/now_u_api/storage.py
@@ -1,9 +1,10 @@
 from storages.backends.azure_storage import AzureStorage
 
-from now_u_api.settings import STATIC_FILES_STORAGE_ACCOUNT_KEY, STATIC_FILES_STORAGE_ACCOUNT_NAME, STATIC_FILES_STORAGE_CONTAINER
+from now_u_api.settings import STATIC_FILES_STORAGE_ACCOUNT_KEY, STATIC_FILES_STORAGE_ACCOUNT_NAME, STATIC_FILES_STORAGE_CONTAINER, STATIC_FILES_STORAGE_DOMAIN
 
 class PublicAzureStorage(AzureStorage):
     account_name = STATIC_FILES_STORAGE_ACCOUNT_NAME
     account_key = STATIC_FILES_STORAGE_ACCOUNT_KEY
     azure_container = STATIC_FILES_STORAGE_CONTAINER
+    custom_domain = STATIC_FILES_STORAGE_DOMAIN
     expiration_secs = None
